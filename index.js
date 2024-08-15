da8d86884bae9ccef34293acff1e84ab7e47753f
chinh 416c <chinh10hht@gmail.com>
2024-08-15 10:20:56 +0700

    upload template

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-var testi = $('#testimonial-slide');
-var owlcarouselslide = function(e) {
-   
-    // testimonial slide
-    testi.owlCarousel({
-        autoplay: false,
-        center: true,
-        loop: true,
-        dots: true,
-        touchDrag: false,
-        mouseDrag: false,
-        smartSpeed: 900,
-        items: 1,
-        margin: 100,
-        nav: true,
-        navText: [
-            '<i class="fa fa-long-arrow-left " aria-hidden="true"></i>',
-            '<i class="fa fa-long-arrow-right " aria-hidden="true"></i>'
-        ],
-        navContainer: '.custom-nav',
-    });
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if(e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
 };
 
-$(document).ready(function() {
-    owlcarouselslide();
-}); 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
